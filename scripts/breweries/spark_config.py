16df95acd18deedeb4c4f9f8281c84130ee6d40a
patrickmartini <28908671+trickmartini@users.noreply.github.com>
2025-02-09 12:28:48 -0300

    RMV deprecated files

--- scripts/breweries/spark_config.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-
-
-def get_spark_session():
-    spark = (SparkSession.builder.appName("breweries transform")
-             .getOrCreate())
-    spark.sparkContext.setLogLevel("INFO")
-    return spark
